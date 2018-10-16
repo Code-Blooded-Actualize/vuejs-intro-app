0bef54630e189ded505924772b68c970fdcae4fd
JackMarx <seriousfools@gmail.com>
2018-10-15 19:26:02 -0500

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <input v-model="message">
+
+    <div v-for="review in reviews">
+      <h2>{{ review.reviewer }} - {{ review.rating }} Stars</h2>
+      <p>{{ review.text }}</p>
+    </div>
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+<style>
+body {
+  background-color: lightblue;
 }
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js! something",
+      reviews: [
+        { 
+          text: "Vue.js is the best thing since sliced bread!",
+          rating: 5,
+          reviewer: "Burton Wax"
+        },
+        { 
+          text: "I love Vue.js, especially when I'm programming!",
+          rating: 4,
+          reviewer: "Emily Post"
+        },
+        { 
+          text: "Vue.js sucks.",
+          rating: 1,
+          reviewer: "Oscar the Grouch"
+        }
+      ]
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
 </script>
